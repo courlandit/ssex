55f9667fa49c182ae8ee781c1eaf1443cd45b823
Kristaps Vitolins <kristaps.vitolins@devlead.lv>
2025-07-20 12:37:52 +0300

    first commit

---
+++ ssex.js
@@ -0,0 +1,116 @@
+(
+    function () {
+        const Attributes = Object.freeze({
+            CLICK: "ssex-click",
+            SUBSCRIBE: "ssex-subscribe",
+            SWAP: "ssex-swap",
+        });
+
+        const Modes = Object.freeze({
+            REPLACE: "replace",
+            APPEND: "append",
+            PREPEND: "prepend",
+        });
+
+        const htmlManipulator = new Map([
+            [Modes.REPLACE, (element, html) => element.innerHTML = html],
+            [Modes.APPEND, (element, html) => element.insertAdjacentHTML("beforeend", html)],
+            [Modes.PREPEND, (element, html) => element.insertAdjacentHTML("afterbegin", html)],
+        ]);
+
+        class SSEX {
+            constructor(options) {
+                this.url = options?.url || "/sse";
+                this.debug = options?.debug || false;
+                this.eventSource = null;
+                this.eventHandlers = {};
+            }
+
+            start() {
+                if (this.eventSource) {
+                    return;
+                }
+
+                this.eventSource = new EventSource(this.url);
+
+                this.eventSource.onerror = (error) => {
+                    this.log("SSEX connection error", error);
+                };
+
+                this.eventSource.onmessage = (event) => {
+                    this.log("Generic message received:", event);
+                };
+
+                this.eventSource.addEventListener("ssex.command", (event) => {
+                    this.handleCommand(JSON.parse(event.data));
+                });
+
+                this.attachDOMListeners();
+            }
+
+            attachDOMListeners() {
+                this.domSubscribers();
+                this.domClicks();
+            }
+
+            domClicks() {
+                document.querySelectorAll("[" + Attributes.CLICK + "]").forEach((element) => {
+                    const url = element.getAttribute(Attributes.CLICK);
+                    element.addEventListener("click", () => {
+                        fetch(url, {
+                            method: "POST",
+                            headers: {"X-SSEX": "true"},
+                        }).then((res) => this.log("ssex-click POST:", url, res.status));
+                    });
+                });
+            }
+
+            domSubscribers() {
+                document.querySelectorAll("[" + Attributes.SUBSCRIBE + "]").forEach((element) => {
+                    const eventName = element.getAttribute(Attributes.SUBSCRIBE);
+                    if (!this.eventHandlers[eventName]) {
+                        this.eventHandlers[eventName] = [];
+
+                        this.eventSource.addEventListener(eventName, (event) => {
+                            this.dispatchToElements(eventName, event.data);
+                        });
+                    }
+
+                    this.eventHandlers[eventName].push(element);
+                });
+            }
+
+            dispatchToElements(eventName, html) {
+                const htmlElements = this.eventHandlers[eventName] || [];
+                htmlElements.forEach((element) => this.manipulateElement(element, html));
+            }
+
+            manipulateElement(element, html) {
+                const mode = element.getAttribute(Attributes.SWAP) || Modes.REPLACE;
+                htmlManipulator.get(mode)(element, html);
+            }
+
+            handleCommand(command) {
+                const {action, target, value} = command;
+                if (action === "alert") {
+                    alert(value);
+                }
+
+                if (action === "redirect") {
+                    window.location.href = value;
+                }
+
+                if (action === "replace") {
+                    document.querySelector(target).innerHTML = value;
+                }
+            }
+
+            log(...args) {
+                if (this.debug) console.log("[SSEX]", ...args);
+            }
+
+        }
+
+        window.SSEX = SSEX;
+    }
+)();
